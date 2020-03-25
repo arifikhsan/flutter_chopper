b50f5d0826f7143fbd9c0e019149d3425df71898
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-03-25 16:45:20 +0700

    up

--- lib/data/post_api_service.dart
+++ lib/data/post_api_service.dart
@@ -3,10 +3,26 @@ import 'package:chopper/chopper.dart';
 part 'post_api_service.chopper.dart';
 
 @ChopperApi(baseUrl: 'https://jsonplaceholder.typicode.com/posts')
-abstract class PostApiSerivce extends ChopperService {
+abstract class PostApiService extends ChopperService {
   @Get()
   Future<Response> getPosts();
 
   @Get(path: '/{id}')
   Future<Response> getPost(@Path('id') int id);
+
+  @Post()
+  Future<Response> postPost(
+    @Body() Map<String, dynamic> body,
+  );
+
+  static PostApiService create() {
+    final client = ChopperClient(
+      baseUrl: 'https://jsonplaceholder.typicode.com',
+      services: [
+        _$PostApiService(),
+      ],
+      converter: JsonConverter(),
+    );
+    return _$PostApiService(client);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,19 @@
-import 'package:chopper/chopper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chopper/data/post_api_service.dart';
+import 'package:flutter_chopper/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
-
-Future<void> getWhat() async {
-  final chopper = ChopperClient(
-    baseUrl: 'https://jsonplaceholder.com',
-    services: [],
-    converter: JsonConverter(),
-  );
-}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+    return Provider(
+      create: (_) => PostApiService.create(),
+      dispose: (_, PostApiService service) => service.client.dispose(),
+      child: MaterialApp(
+        title: 'Material App',
+        home: HomePage(),
       ),
     );
   }
